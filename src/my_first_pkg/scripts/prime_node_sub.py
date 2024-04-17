7c295bae6dcc2f579ae99078a40d3e446819136d
manavsikkas <54614768+manavsikkas@users.noreply.github.com>
2024-04-18 09:26:16 +1000

    Add files via upload

---
+++ src/my_first_pkg/scripts/prime_node_sub.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+def pub_call(text):
+    rospy.loginfo("It's a prime Number")
+
+if __name__ == '__main__':
+    rospy.init_node("prime_node_sub")
+    rospy.Subscriber("numbering", String, pub_call)
+    rospy.spin()
+    rospy.logerr("The connection is severed")
